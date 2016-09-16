c1ef08c99ee812a3250f3e9d139b5b3a730b2769
Student8 <Student8@10.8.33.151>
2016-09-16 12:34:25 -0400

    Added OOPExample

---
+++ AfsanaIntroduction/src/introUnit/OOPExample.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package introUnit;
+
+/**
+ * @author Student8
+ * This class us designed to contrast with the ProceduralExample.
+ * It embodies an Object-Oriented approach
+ */
+public class OOPExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//construct a student
+		Student jillian = new Senior("Jillian");
+		Student joseph = new Student("Joseph");
+		Student jordan = new Student("Jordan");
+		jillian.talk();
+		joseph.talk();
+		jordan.talk();
+
+	}
+
+}
--- AfsanaIntroduction/src/introUnit/ProceduralExample.java
+++ AfsanaIntroduction/src/introUnit/ProceduralExample.java
@@ -8,8 +8,12 @@ package introUnit;
  *
  */
 public class ProceduralExample {
+	
 	public static void main(String[]args){
 		String[] names = {"Jillian","Joseph","Jordan"};
-		System.out.println("My name is "+ names [0] + "." );
+		String[] years = {"Senior", "Freshmen", "Junior"};
+		System.out.println("Hi, my name is "+names [0]+ " and I am " +years ".");
+		System.out.println("Hi, my name is "+names [1]+ " and I am " +years ".");
+		System.out.println("Hi, my name is "+names [2]+ " and I am " +years ".");
 	}
 }
