e2b94094ba276dff83dffa2888408305d29f4ad7
Student8 <Student8@10.8.33.151>
2016-09-19 13:14:41 -0400

    datetypes new edits

--- AfsanaIntroduction/src/dataType/DataTypes.java
+++ AfsanaIntroduction/src/dataType/DataTypes.java
@@ -11,6 +11,8 @@ public class DataTypes {
 
 	public static void main(String[] args) {
 		System.out.println(5.0/2); //double divided by int
+		System.out.println((double)5/2); //double divided by int
+		System.out.println(5/2); //double divided by int
 	}
 
 }
