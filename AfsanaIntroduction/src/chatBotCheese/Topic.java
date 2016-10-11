cd0e66567fd0fbdd1519cd6b66566daadb77f71a
Student8 <Student8@10.8.33.151>
2016-10-11 13:18:51 -0400

    arrays

--- AfsanaIntroduction/src/chatBotCheese/Topic.java
+++
@@ -1,6 +0,0 @@
-package chatBotCheese;
-
-public interface Topic {
-	public void talk();
-	public boolean isTriggered(String userInput);
-}
---
+++ AfsanaIntroduction/src/chatBox/AfsanaHello.java
@@ -0,0 +1,17 @@
+package chatBox;
+
+public class AfsanaHello implements Topic {
+
+	@Override
+	public void talk() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isTriggered(String userInput) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
