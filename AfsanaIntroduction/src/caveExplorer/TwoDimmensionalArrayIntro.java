18e1ed525ff321976aab450aa09df2d11aeccf63
Student7 <Student7@10.8.33.151>
2016-10-31 12:32:51 -0400

    two dimmensional array

---
+++ AfsanaIntroduction/src/caveExplorer/TwoDimmensionalArrayIntro.java
@@ -0,0 +1,13 @@
+package caveExplorer;
+
+public class TwoDimmensionalArrayIntro {
+
+	public static void main(String[] args) {
+		//rows,columns
+		String[][] array2D = new String[4][3];
+		//iterate row by row
+		for(int column = 0; column < array2D[row].length; column++){
+			array2D[row][column] = "(")
+	}
+
+}
