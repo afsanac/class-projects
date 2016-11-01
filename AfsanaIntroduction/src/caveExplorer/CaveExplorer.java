2161a332b94e1978cc21ff9b80ac7c8710d54f63
Student7 <Student7@10.8.33.151>
2016-11-01 12:32:04 -0400

    game

---
+++ AfsanaIntroduction/src/caveExplorer/CaveExplorer.java
@@ -0,0 +1,62 @@
+package caveExplorer;
+
+import java.util.Scanner;
+
+public class CaveExplorer {
+	public static CaveRoom[][] caves;
+	private static Scanner in;
+	public static CaveRoom currentRoom;
+	private Inventory inventory;
+	
+	public static void main(String[] args){
+		in = new Scanner(System.in);
+		new CaveExplorer();
+	}
+	
+	
+	public CaveExplorer(){
+		caves = new CaveRoom[5][5];
+		for(int i = 0; i < caves.length; i++){
+			for(int j = 0; j < caves[i].length; j++){
+				caves[i][j] = new CaveRoom("This cave has coordinates "+i+", "+j);
+			}
+		}
+		currentRoom = caves[3][3];
+		currentRoom.enter();
+		caves[3][3].setConnection(CaveRoom.EAST, caves[3][4], new Door());
+		caves[3][3].setDescription("This is the room you started in.");
+		
+		caves[3][4].setConnection(CaveRoom.NORTH, caves[2][4], new Door());
+		caves[2][4].setConnection(CaveRoom.WEST, caves[2][3], new Door());
+		caves[2][3].setConnection(CaveRoom.SOUTH, caves[3][3], new Door());
+		inventory = new Inventory(caves);
+		startExploring();
+	}
+	
+	private void startExploring() {
+		while(true){
+			print(inventory.getDescription());
+			System.out.println(currentRoom.getDescription());
+			System.out.println("What would you like to do?");
+			String input = waitForInput();
+			act(input);
+		}
+	}
+	
+	public static void print(String s){
+		System.out.println(s);
+	}
+
+
+	private void act(String input) {
+		
+		currentRoom = currentRoom.interpretAction(input);
+	}
+
+
+	public static String waitForInput(){
+		return in.nextLine();
+	}
+	
+}
+
--- AfsanaIntroduction/src/caveExplorer/TwoDimmensionalArrayIntro.java
+++ AfsanaIntroduction/src/caveExplorer/TwoDimmensionalArrayIntro.java
@@ -1,13 +1,34 @@
 package caveExplorer;
 
+import java.util.Arrays;
+
 public class TwoDimmensionalArrayIntro {
 
 	public static void main(String[] args) {
-		//rows,columns
-		String[][] array2D = new String[4][3];
-		//iterate row by row
-		for(int column = 0; column < array2D[row].length; column++){
-			array2D[row][column] = "(")
+		String[] xox = {"x","o","x","o","x"};
+		System.out.println(Arrays.toString(xox));
+		//a 1D array prints a horizontal string
+		
+		String[][] arr2D = new String[5][4];
+		System.out.println("The height is "+arr2D.length);
+		System.out.println("The width is "+arr2D[0].length);
+		
+		for(int row = 0; row < arr2D.length; row++){
+			
+			//populate with coordinates
+			for(int col = 0; col < arr2D[row].length; col++){
+				arr2D[row][col] = "("+row+", "+col+")";
+			}
+			
+		}
+		
+		//print the 2D array
+		//Every element in a 2D array IS itself an array,
+		//so a for-each loop looks like this:
+		for(String[] row : arr2D){
+			System.out.println(Arrays.toString(row));
+		}
+		
 	}
 
 }
