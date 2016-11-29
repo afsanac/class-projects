33201259868d78fcfe5fdf6f6abe54f666ac707b
Student7 <Student7@10.8.33.151>
2016-11-29 12:32:08 -0500

    pokemon

---
+++ AfsanaIntroduction/src/sort/pokemon.java
@@ -0,0 +1,27 @@
+package sort;
+
+public class pokemon {
+	private int level;
+	private int hp;
+	private String name;
+	private boolean poisoned;
+	
+	public pokemon(String name, int level){
+		hp = 100;
+		this.name = name;
+		this.level = level;
+		this.poisoned = false;
+	}
+	
+	public void attack(){
+		
+	}
+	
+	
+	
+	public
+	public void lapsed(){
+		if(poisoned)hp-=15;
+	}
+	
+}
