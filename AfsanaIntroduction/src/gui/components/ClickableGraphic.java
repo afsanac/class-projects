0567b556a74676b9e3d918fcd80d6574fe6364ee
Student7 <Student7@10.8.33.151>
2016-12-23 12:31:04 -0500

    Lickable Screen and Graphics

---
+++ AfsanaIntroduction/src/gui/components/ClickableGraphic.java
@@ -0,0 +1,9 @@
+package gui.components;
+
+public class ClickableGraphic {
+
+	public ClickableGraphic(int x, int y, int w, int h, String imageLocation) {
+		super(x, y, w, h, imageLocation)
+	}
+
+}
